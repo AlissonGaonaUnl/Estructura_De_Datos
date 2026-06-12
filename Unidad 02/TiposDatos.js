24ad53f633e60a9e9a7c40d686c4751191797fa8
Alisson Gaona <alisson.gaona@unl.edu.ec>
2026-06-12 18:08:44 -0500

    Hi.World, Tipos de Datos y String

---
+++ Unidad 02/TiposDatos.js
@@ -0,0 +1,22 @@
+//Primitivos 
+let nombre = "Alison"; //String
+let edad = 20; //Number
+let esMayorDeEdad = true; //Boolean 
+let noHayValor = null; //Null
+let noDefinido = undefined; //Undefined
+let simboloUnico = Sumbol("unico"); //Symbolo valor unico
+let numeroGrnde = 2n; //BigInt Datos Gigantes 
+
+//Completos
+//Objeto
+let carro = {
+    marca: "Kia",
+    modelo: "Picanto",
+    año: 2020
+};
+//Arrays 
+let frutas = ["Manzana", "Banana", "Naranja"];
+//Funciones
+function saludar() {
+    
+}
