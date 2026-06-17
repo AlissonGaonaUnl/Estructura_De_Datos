63e0817a890431654dc445f3eec50a341879e425
Alisson Gaona <alisson.gaona@unl.edu.ec>
2026-06-17 10:23:58 -0500

    Tarea del dia 17/06/2026

--- Unidad 02/00_JavaScript/clase-condicional.js
+++ Unidad 02/00_JavaScript/clase-condicional.js
@@ -1,3 +1,18 @@
 /*
 Condicinales 
 */
+
+
+if (5===5) {
+    console.log( 'Es igual' )
+} else {
+    console.log('No es igual') 
+}
+
+let queso = "Mozarela";
+
+if (queso) {
+    console.log("¡Siii! Hay queso para hacer tostadas con queso.");
+} else {
+    console.log("No hay tostadas con queso para ti hoy :(.");
+}
